fdc717865bc1f279cf5cece3316b81d0f3e108b8
Azeem Khan <98760240+Azeemo@users.noreply.github.com>
2023-10-05 12:11:15 +0530

    Update _13_FractionClass.cpp

--- _13_FractionClass.cpp
+++ _13_FractionClass.cpp
@@ -7,15 +7,30 @@ class Fraction {
       int denominator;
 
   public:
+    // we have created a prametized constructor
+    // we dont want fraction to contain garbage values
+
     Fraction(int numerator,int denominator){
         this->numerator = numerator;
         this->denominator = denominator;   /// mandatory
     }
+
+// here object will call the print function
+    // and its address will be stored in this keyword
     void print(){
-      cout<<numerator<<"/"<<denominator<<endl; /// optional
+      cout<<this->numerator<<"/"<<denominator<<endl; /// optional
     }
-    void add(Fraction f2){
-       int lcm = denominator * f2.denominator;  /// optional
+
+// HERE IS THE ADD FUNCTION
+    // THIS FUNCTION WILL ADD F2 IN F1
+    // F1 = F1+F2 AND F2 REMAIN unchanged
+    //  HeRE we will pass f2 as argument 
+    // f1 is stored in this keyword
+
+// PURELY LOGICAL 
+    // x and y created to find numerator
+    void add(Fraction const &f2){
+       int lcm = this->denominator * f2.denominator;  /// optional
        int x = lcm/this->denominator;   /// optional
        int y = lcm/f2.denominator;
        int num = x*numerator + y*f2.numerator;
@@ -25,7 +40,17 @@ class Fraction {
        this->denominator = lcm;
 
        simplify();
+       // actually it is this->simplify()
     }
+
+    void multiply(Fraction const &f2){
+      int num = this->numerator*f2.numerator;
+      int den = this->denominator*f2.denominator;
+      this->numerator=num;
+      this->denominator=den;
+      simplify();
+    }
+
     void simplify(){
        int gcd = 1;
        int j = min(this->numerator,this->denominator);  /// optional
@@ -42,14 +67,29 @@ class Fraction {
     denominator = denominator/gcd;
     }
 
-};
+} ;
 
 int main(){
+
+  /// WE HAVE ADDED CONST &F2 IN THE FUNCTION
+  // THIS IS BCAUSE WE DONT WANT ANYONE TO CHANGE THE VALUE OF F2
+  // we not want f2=-1
+  // so in order to avoid this we pass CONST &F2 
+  //this WILL AVOID CHANGE IN THE FRACTION F2
+
    Fraction f1(10,2);
    Fraction f2(15,4);
 
    f1.add(f2);
    f1.print();
    f2.print();
+
+ Fraction f3(10,2);
+   Fraction f4(15,4);
+   f3.multiply(f4);
+    f3.print();
+   f4.print();
+
+
    return 0;
 }
