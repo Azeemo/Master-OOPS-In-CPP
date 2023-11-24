b42ea8612e0a8303bf6a6f4745cb8276a24980e3
Azeem Khan <98760240+Azeemo@users.noreply.github.com>
2023-11-24 15:09:04 +0530

    Create _29_Compile_Time_Polymorphism.cpp

---
+++ _29_Compile_Time_Polymorphism.cpp
@@ -0,0 +1,35 @@
+Compile-time polymorphism, also known as static polymorphism or method overloading,
+occurs when multiple methods in a class have the same name but different parameters 
+(either different types or a different number of parameters). 
+  The decision about which method to call is made by the compiler at compile time.
+
+public class MathOperations {
+    // Method to add two integers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method to add three integers
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Method to concatenate two strings
+    public String add(String str1, String str2) {
+        return str1 + str2;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MathOperations math = new MathOperations();
+
+        int result1 = math.add(2, 3);              // Calls the first add method
+        int result2 = math.add(2, 3, 5);           // Calls the second add method
+        String result3 = math.add("Hello, ", "World!"); // Calls the third add method
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+    }
+}
